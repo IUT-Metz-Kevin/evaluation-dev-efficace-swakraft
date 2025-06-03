ba0c4d2e52ea9db11e3f74385a0b9628e40909f2
Hugo Beets <hugo.beets2003@gmail.com>
2025-06-03 13:46:21 +0200

    RED

---
+++ sweeper.test.ts
@@ -0,0 +1,11 @@
+import { assertEquals } from "jsr:@std/assert/equals";
+
+
+function minesweeper(grid: string) {
+    return "";
+}
+
+
+Deno.test("point simple sans mine", () => {
+    assertEquals(".", "0")
+});
--- sweeper.ts
+++
@@ -1,8 +0,0 @@
-import { assertEquals } from "jsr:@std/assert/equals";
-
-
-
-
-
-
-
