745d62dbc40271fce3930c7294daf664694bc36c
Hugo Beets <hugo.beets2003@gmail.com>
2025-06-03 14:38:13 +0200

    RED

--- sweeper.test.ts
+++ sweeper.test.ts
@@ -1,4 +1,5 @@
 import { assertEquals } from "jsr:@std/assert/equals";
+import { minesweeper } from "./temp";
 
 
 function minesweeper(grid: string): string {
@@ -24,3 +25,7 @@ Deno.test("simple mine", () => {
 Deno.test("grille 1D", () => {
     assertEquals(minesweeper(".*.**."), "1*2**1")
 });
+
+Deno.test("gille 2D 2 lignes", () => {
+    assertEquals(minesweeper(".*.**.\n....*.").split("\n")[0], "1*2**2")
+})
