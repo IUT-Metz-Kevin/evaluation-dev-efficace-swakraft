e13de55b22571f2687c1d5f71891f39203d2122e
Hugo Beets <hugo.beets2003@gmail.com>
2025-06-03 14:32:55 +0200

    GREEN

--- sweeper.test.ts
+++ sweeper.test.ts
@@ -2,15 +2,50 @@ import { assertEquals } from "jsr:@std/assert/equals";
 
 
 function minesweeper(grid: string): string {
-    const r = grid.split("").map((character, i, arr) => {
-        if (character === "*") return "*";
-        let count = 0;
-        if (arr[i - 1] === "*") count++;
-        if (arr[i + 1] === "*") count++;
-        return count.toString();
-    }).join("");
     
-    return r;
+    const arrayGrid: string[][] = grid.split("\n").map((line) => line.split(""))
+    //console.log(arrayGrid); // décomposition en tableau 2D réel
+
+    let r = "";
+
+    for (let x = 0; x < arrayGrid.length; x++) {
+        const currentLine = arrayGrid[x];
+        const nextLine = arrayGrid[x+1];
+        console.log(currentLine)
+        for (let y = 0; y < currentLine.length; y++) {
+            const currentCharacter = arrayGrid[x][y]
+            console.log(currentCharacter)
+
+            if (currentCharacter === "*") {
+                r += "*";
+            } else {
+                let count = 0;
+    
+                if (currentLine[y - 1] === "*") count++; // à gauche
+                if (currentLine[y + 1] === "*") count++; // à droite
+                if (nextLine) {
+    
+                
+                if (nextLine[y - 1] === "*") count++ // en bas à gauche
+                if (nextLine[y] === "*") count++ // en dessous
+                if (nextLine[y + 1] === "*") count++ // en bas à droite
+                }
+                r += count;
+            }
+        }
+        r += "\n";
+    }
+
+
+    // const r = grid.split("").map((character, i, arr) => {
+    //     if (character === "*") return "*";
+    //     let count = 0;
+    //     if (arr[i - 1] === "*") count++;
+    //     if (arr[i + 1] === "*") count++;
+    //     return count.toString();
+    // }).join("");
+    
+    return r.trim();
 }
 
 
@@ -25,3 +60,7 @@ Deno.test("simple mine", () => {
 Deno.test("grille 1D", () => {
     assertEquals(minesweeper(".*.**."), "1*2**1")
 });
+
+Deno.test("gille 2D 2 lignes", () => {
+    assertEquals(minesweeper(".*.**.\n....*.").split("\n")[0], "1*2**2")
+})
