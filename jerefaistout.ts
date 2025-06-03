f7eaf136f44d7c1dac1011826db141bc4de0d287
Hugo Beets <hugo.beets2003@gmail.com>
2025-06-03 15:13:28 +0200

    RED

---
+++ jerefaistout.ts
@@ -0,0 +1,10 @@
+import { assertEquals } from "jsr:@std/assert/equals";
+
+
+function minesweeper(grid: string): string {
+    
+}
+
+Deno.test("grille vide", () => {
+    assertEquals(minesweeper(""), "")
+})
